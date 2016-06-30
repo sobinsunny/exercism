dcaaa48eb0c7aad37627c94e6b6ecf587df63b01
Sobin <sobisunny@gmail.com>
2016-06-30 22:56:22 +0530

    phone number problem fixed test upto 9

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -5,11 +5,17 @@ def initialize(number)
 end
 def number
 	if @number[/[a-zA-Z]/]
-		number = "0"*@number.scan(/\d/).count
+		number = "0"*@number.scan(/\w/).count
+	elsif @number.size < 10
+		number = "0"*10
+	elsif @number[/\d*/].size > 10
+		@number[0]=''  if @number[0]=='1'
+		return "0"*10  if @number.size>10
+		number = @number
 	else	
-		number = @number.gsub(/\D/,"").to_i
+		number = @number.gsub(/\D/,"")
 	end
-	   number.to_i
+	   number
 end
 
 end
