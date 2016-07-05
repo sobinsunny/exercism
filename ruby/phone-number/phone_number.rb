3498d30cd7912c31586cf0154048cd01c76c0d8d
Sobin <sobisunny@gmail.com>
2016-07-05 21:08:50 +0530

    Phone number verification

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,21 +1,24 @@
 class PhoneNumber
+  def initialize(number)
+    @number = number.to_s
+  end
 
-def initialize(number)
-	@number = number.to_s
-end
-def number
-	if @number[/[a-zA-Z]/]
-		number = "0"*@number.scan(/\w/).count
-	elsif @number.size < 10
-		number = "0"*10
-	elsif @number[/\d*/].size > 10
-		@number[0]=''  if @number[0]=='1'
-		return "0"*10  if @number.size>10
-		number = @number
-	else	
-		number = @number.gsub(/\D/,"")
-	end
-	   number
-end
+  def number
+    if @number.size < 10
+      number = '0' * 10
+    elsif @number[/\d*/].size > 10
+      @number[0] = ''  if @number[0] == '1'
+      return '0' * 10  if @number.size > 10
+      number = @number
+    elsif @number[/[a-zA-Z]/]
+      number = '0' * 10
+    else
+      number = @number.gsub(/\D/, '')
+    end
+    number
+  end
 
+  def area_code
+    number.chars.first(3).join('')
+  end
 end
